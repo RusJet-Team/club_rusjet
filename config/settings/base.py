2f5c948a874ef81e21d498111757c0eb4fc318fe
Konstantin Raikhert <raikhert13@gmail.com>
2022-04-19 23:16:08 +0300

    add main_page

--- config/settings/base.py
+++ config/settings/base.py
@@ -43,7 +43,9 @@ DJANGO_APPS = [
 ]
 THIRD_PARTY_APPS = []
 
-LOCAL_APPS = []
+LOCAL_APPS = [
+    "apps.main_page",
+]
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
