58845d538a000a4d6961ae053b83efbfa9ca67aa
Konstantin Raikhert <69113745+KonstantinRaikhert@users.noreply.github.com>
2022-04-26 01:51:42 +0300

    Develop (#13)
    * start project
    
    * add pre-commit
    
    * project settings
    
    * new app general_page and first view
    
    * template for main page
    
    * add migrations for images
    
    * workfow build image
    
    * add compose
    
    * add compose
    
    * delete general_page app
    
    * add main_page
    
    * test deploy
    
    * CI/CD
    
    * fix Dockerfile
    
    * bugfix deploy
    
    * bugfix deploy
    
    * bugfix deploy
    
    * bugfix deploy
    
    * bugfix deploy
    
    * test
    
    * test
    
    * test
    
    * dev template

--- apps/main_page/urls.py
+++ apps/main_page/urls.py
@@ -3,8 +3,9 @@
 # from django.contrib import admin
 from django.urls import path
 
-from apps.main_page.views import HomePageView
+from apps.main_page.views import HomePageView, PortfolioDetailsView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="index"),
+    path("portfolio/", PortfolioDetailsView.as_view(), name="portfolio"),
 ]
--- apps/main_page/views.py
+++ apps/main_page/views.py
@@ -4,7 +4,17 @@ from django.views.generic.base import TemplateView
 
 class HomePageView(TemplateView):
 
-    template_name = "base.html"
+    template_name = "develop.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['latest_articles'] = Article.objects.all()[:5]
+        return context
+
+
+class PortfolioDetailsView(TemplateView):
+
+    template_name = "portfolio-details.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
