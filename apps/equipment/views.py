f5a6595da11712aaa5c95cd8cbc1dba7e7474c4f
Konstantin Raikhert <raikhert13@gmail.com>
2022-05-31 00:27:00 +0300

    to deploy rc-open

---
+++ apps/equipment/views.py
@@ -0,0 +1,22 @@
+# from django.shortcuts import get_object_or_404
+from django.views.generic.base import TemplateView
+
+
+class EquipmentView(TemplateView):
+
+    template_name = "equipment/equipment.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context["achievements"] = get_object_or_404(HalfStaticPage, slug="achievements")
+        return context
+
+
+class ShopView(TemplateView):
+
+    template_name = "equipment/list_shop.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context["achievements"] = get_object_or_404(HalfStaticPage, slug="achievements")
+        return context
