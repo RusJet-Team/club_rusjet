fe06ac1245a49e25b655c753cdb5787ce348e7bf
Konstantin Raikhert <69113745+KonstantinRaikhert@users.noreply.github.com>
2022-06-21 00:15:46 +0300

    Develop (#25)
    * start project
    
    * add pre-commit
    
    * project settings
    
    * new app general_page and first view
    
    * template for main page
    
    * add migrations for images
    
    * workfow build image
    
    * add compose
    
    * add compose
    
    * delete general_page app
    
    * add main_page
    
    * test deploy
    
    * CI/CD
    
    * fix Dockerfile
    
    * bugfix deploy
    
    * bugfix deploy
    
    * bugfix deploy
    
    * bugfix deploy
    
    * bugfix deploy
    
    * test
    
    * test
    
    * test
    
    * dev template
    
    * add carousel and partners
    
    * add team and services
    
    * service template
    
    * services update
    
    * about us update
    
    * add news models and templates
    
    * write factories and filldb command
    
    * fabrics, news on main page
    
    * almost ready news
    
    * add portfolio projects
    
    * static pages
    
    * deploy change
    
    * collapse migrations
    
    * bagfix in css
    
    * add favicon on admin panel
    
    * change user in deploy.service
    
    * to deploy rc-open
    
    * change certprovider to letsencrypt
    
    * add templates for equipment
    
    * add new templates, change partners
    
    * final rendering templates
    
    * change relations to service and projects
    
    * add image_preview in mixin AdminImagePreview
    
    * add equipments templates and urls
    
    * complete equip templates
    
    * add js in news admin
    
    * admin panel
    
    * fix statig page Achievements

--- apps/club/urls.py
+++ apps/club/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from apps.club.views import ClubMembersView, HistoryView, PerformancesView
+from apps.club.views import AchievementsView, ClubMembersView, HistoryView
 
 urlpatterns = [
     path("history/", HistoryView.as_view(), name="history"),
     path("members/", ClubMembersView.as_view(), name="members"),
-    path("performances", PerformancesView.as_view(), name="performances"),
+    path("achievements/", AchievementsView.as_view(), name="achievements"),
 ]
